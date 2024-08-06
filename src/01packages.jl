aefaccff1a3b43f2d7a2b4260028dda356a84e00
Arturo Erdély <arturoerdely@gmail.com>
2024-08-06 09:54:00 -0600

    updates

---
+++ src/01packages.jl
@@ -0,0 +1,22 @@
+#=
+
+    Install some external packages needed for the Risk Theory course using a loop. 
+    They can also be installed one by one from the package mode in the terminal 
+    using the `add` command followed by the package name.
+
+=#
+
+using Pkg # calling the Julia installer package
+
+begin
+    package = ["QuadGK", "HCubature", "Optim",
+               "SpecialFunctions", "LaTeXStrings", 
+               "Distributions", "StatsBase",
+               "Plots", "StatsPlots"
+    ]
+    for p in package
+        println("*** Installing package: ", p)
+        Pkg.add(p)
+    end
+    println("*** End of list of packages.")
+end
